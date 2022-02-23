5f97c07506458a1fe3c5c872a264fad3a7000b3f
Gutierre Braga <Gutierre_Braga@outlook.com>
2022-02-23 20:16:36 -0300

    Finish the rails 7.0.2.2 guidegit add .!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@
 
 # This Class create an Article
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'dhh', password: 'secret', except: %i[index show]
+
   def index
     @articles = Article.all
   end
@@ -48,6 +50,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :status)
   end
 end
